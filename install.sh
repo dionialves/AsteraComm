ac7a7aa41352348142e54a97ab163cad2bd2fd51
Dioni Alves de Oliveira <dionialves@gmail.com>
2025-06-18 11:27:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,16 @@ cd AsteraComm
 
 # Baixar e extrair o frontend
 echo "Baixando o frontend..."
-mkdir -p nginx/www
-cd nginx/www
-wget https://github.com/dionialves/AsteraComm-frontend/releases/download/v0.1.1/AsteraComm-0.1.1.zip
-unzip AsteraComm-0.1.1.zip
-rm -f AsteraComm-0.1.1.zip
+mkdir frontend/app
+cd frontend/app
+git clone https://github.com/dionialves/AsteraComm-frontend.git .
 cd ../../
 
 # Baixar o backend
 echo "Baixando o backend..."
 mkdir -p asteracomm/app
 cd asteracomm/app
-wget https://github.com/dionialves/AsteraComm-backend/releases/download/v0.1.2/AsteraComm-0.1.2.jar
-mv AsteraComm-*.jar AsteraComm.jar
+git clone https://github.com/dionialves/AsteraComm-backend.git .
 cd ../..
 
 # Build e up do Docker
